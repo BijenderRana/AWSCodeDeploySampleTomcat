d5b502d3436b277687b9194e46c131cc6275e979
BijenderRana <82703249+BijenderRana@users.noreply.github.com>
2023-02-02 14:59:47 +0530

    Delete scripts/scripts directory

--- scripts/scripts/after_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export versioning=77518421
-chown tomcat:tomcat -R /opt/tomcat/webapps
-cp /opt/tomcat/webapps/LoginWebApp-1.war /opt/war-backup/$(date +"%Y-%m-%d")/$versioning-LoginWebApp-1.war
--- scripts/scripts/application_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-systemctl start tomcat
-sleep 60
